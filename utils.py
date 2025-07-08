5115b27ab1f12528461dea5b65467bb80999c45b
Eugene Yu <eugene.yu@nordicsemi.no>
2025-07-09 08:06:08 +0900

    update: version update to v1.7.1

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ LIGHT_CSS_NAME              = "light"
 DARK_CSS_NAME               = "dark"
 RESOURCES_DIR               = "resources"
 
-APP_VERSION                 = "1.7.0"
+APP_VERSION                 = "1.7.1"
 COMMANDS_PREDEFINED_FILE1   = "atcmder_predefined_cmd_1.yaml"
 COMMANDS_PREDEFINED_FILE2   = "atcmder_predefined_cmd_2.yaml"
 COMMANDS_PREDEFINED_FILE3   = "atcmder_predefined_cmd_3.yaml"
